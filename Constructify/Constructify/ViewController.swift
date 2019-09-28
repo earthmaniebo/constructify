478f74c0cc971e5a2cc46d8a60c06fbbda0a6a1a
Earth Maniebo <earthmaniebo@gmail.com>
2019-09-28 11:31:54 +0800

    Add podfile

---
+++ Constructify/Constructify/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Constructify
+//
+//  Created by Earth Maniebo on 28/09/2019.
+//  Copyright © 2019 Earth Maniebo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
