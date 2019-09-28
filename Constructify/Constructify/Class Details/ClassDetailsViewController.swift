be0eae8b27d3c67f381b20b5ecc6ad5fefbaad08
Earth Maniebo <earthmaniebo@gmail.com>
2019-09-29 05:24:58 +0800

    Done with class detail screen

--- Constructify/Constructify/Class Details/ClassDetailsViewController.swift
+++ Constructify/Constructify/Class Details/ClassDetailsViewController.swift
@@ -1,19 +1,20 @@
 //
-//  ProjectDetailsViewController.swift
+//  ClassDetailsViewController.swift
 //  Constructify
 //
-//  Created by Earth Maniebo on 28/09/2019.
+//  Created by Earth Maniebo on 29/09/2019.
 //  Copyright © 2019 Earth Maniebo. All rights reserved.
 //
 
 import UIKit
 
-class ProjectDetailsViewController: UIViewController {
+class ClassDetailsViewController: UIViewController {
 
     @IBAction func didTapBack(_ sender: Any) {
-        self.navigationController?.popViewController(animated: true)
+        navigationController?.popViewController(animated: true)
     }
-    @IBOutlet weak var projectNameLabel: UILabel!
+    @IBOutlet weak var classNameLabel: UILabel!
+    @IBOutlet weak var typeNameLabel: UILabel!
     @IBOutlet weak var collectionView: UICollectionView!
     
     override func viewDidLoad() {
@@ -24,51 +25,61 @@ class ProjectDetailsViewController: UIViewController {
         collectionView.register(UINib(nibName: "ItemCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: "itemCell")
         collectionView.translatesAutoresizingMaskIntoConstraints = false
     }
-        
     
-    override var preferredStatusBarStyle: UIStatusBarStyle {
-        return .lightContent
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
     }
-    
+    */
+
 }
 
 
-extension ProjectDetailsViewController: UICollectionViewDataSource {
+extension ClassDetailsViewController: UICollectionViewDataSource {
     
     func numberOfSections(in collectionView: UICollectionView) -> Int {
         return 1
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 3
+        return 4
     }
     
     func collectionView(_ collectionView: UICollectionView,
                         cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "itemCell", for: indexPath) as! ItemCollectionViewCell
         cell.progressBar.borderWidth = 0
+        cell.percentageLabel.isHidden = true
         switch indexPath.item {
         case 0:
-            cell.progressBar.setProgress(0.96, animated: true)
-            cell.progressBar.primaryColor = ConColors.greenProgress.uiColor
-            cell.nameLabel.text = "Electrical"
-            cell.percentageLabel.text = "96%"
-            cell.outOfLabel.text = "3000 of 3125"
-        case 1:
-            cell.progressBar.setProgress(0.30, animated: true)
+            cell.progressBar.setProgress(0.20, animated: true)
             cell.progressBar.primaryColor = ConColors.orangeProgress.uiColor
-            cell.nameLabel.text = "Carpentry"
-            cell.percentageLabel.text = "30%"
+            cell.nameLabel.text = "Halloween Pumpkins"
+            cell.outOfLabel.text = "10 of 50"
+            cell.imageView.image = UIImage(named: "pumpkins")
+        case 1:
+            cell.progressBar.setProgress(0.60, animated: true)
+            cell.progressBar.primaryColor = ConColors.orangeProgress.uiColor
+            cell.nameLabel.text = "Grassy Leaves"
             cell.outOfLabel.text = "30 of 100"
-            cell.imageView.image = UIImage(named: "carpentry")
+            cell.imageView.image = UIImage(named: "grassy_leaves")
         case 2:
-            cell.progressBar.setProgress(0.10, animated: true)
+            cell.progressBar.setProgress(0.06, animated: true)
             cell.progressBar.primaryColor = ConColors.redProgress.uiColor
-            cell.nameLabel.text = "Miscellaneous"
-            cell.percentageLabel.text = "10%"
-            cell.outOfLabel.text = "100 of 1000"
-            cell.imageView.image = UIImage(named: "misc")
-            cell.replenishButton.isHidden = false
+            cell.nameLabel.text = "Money Plants"
+            cell.outOfLabel.text = "60 of 900"
+            cell.imageView.image = UIImage(named: "money_plants")
+        case 3:
+            cell.progressBar.setProgress(0.0, animated: true)
+            cell.progressBar.primaryColor = .clear
+            cell.nameLabel.text = "Thistle"
+            cell.outOfLabel.text = "0 of 10"
+            cell.imageView.image = UIImage(named: "thistle")
         default:
             print()
         }
@@ -77,7 +88,7 @@ extension ProjectDetailsViewController: UICollectionViewDataSource {
 }
 
 
-extension ProjectDetailsViewController: UICollectionViewDelegate {
+extension ClassDetailsViewController: UICollectionViewDelegate {
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         if indexPath.item == 2 {
@@ -87,7 +98,7 @@ extension ProjectDetailsViewController: UICollectionViewDelegate {
 }
 
 
-extension ProjectDetailsViewController: UICollectionViewDelegateFlowLayout {
+extension ClassDetailsViewController: UICollectionViewDelegateFlowLayout {
     
     func collectionView(_ collectionView: UICollectionView,
                         layout collectionViewLayout: UICollectionViewLayout,
